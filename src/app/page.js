1e2d9561f033a4951451c1dd5451dea3ba0bb06c
ibrahim <ibrahimsayed7410@gmail.com>
2023-12-25 22:25:41 +0200

    center image

--- src/app/page.js
+++ src/app/page.js
@@ -58,7 +58,7 @@ function swichIcon(bool,num){
    
  <div >
   {
-  pageWidth<=375?<Image src={myimge1} alt="" className="mx-auto" ></Image>:<Image src={myimge} alt="" className="mx-auto"></Image>
+  pageWidth<=375?<Image src={myimge1} alt="" className="mx-auto d-flex" ></Image>:<Image src={myimge} alt="" className="mx-auto d-flex"></Image>
   }
  
   <div className={` mx-auto centerAcc ${pageWidth<=375?"w-90 mt-132":"w-75 mt-220"} `}  >
